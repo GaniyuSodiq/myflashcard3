58a3bcdd4163416e1d98a4f3b48bf73a86b22fec
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-01-31 12:00:18 +0100

    complete the initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+const questionsList = [
+  {
+    id: 1,
+    question: "what is access bank vision statement",
+    answer: "To be the world's most respected african bank",
+  },
+  {
+    id: 2,
+    question: "what is access bank mission statement",
+    answer:
+      "Setting standards for sustainable business practices that unleash the talents of our employees, deliver superior value to our customers and provide innovative solutions for the markets and communities we serve",
+  },
+  {
+    id: 3,
+    question: "what is access bank Core Values",
+    answer:
+      "Leadership - Excellence - Empowered employees - Passion for customers - Professionalism - Innovation",
+  },
+];
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
